0ea3c160cd88da5ef92f1f55ea46ed72a79ecea0
Romeu Palos de Gouvêa <romeupalos@freegatelabs.com>
2016-10-13 01:37:27 -0300

    Changing install script to be more verbose

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-python -c "import pystr" 2> /dev/null
-if [ $? -ne 0 ]; then
+python -c "import pysrt" 2> /dev/null
+if [ "$?" -ne 0 ]; then
     echo "Installing pysrt."
     sudo apt-get install python-pysrt
 fi
 
+echo "Installing horus to /usr/bin"
 sudo install horus.py /usr/bin/horus
+
+echo "Installing nautilus script"
 install nautilus_script "$HOME/.local/share/nautilus/scripts/Download Subtitles"
+
+echo "Done."
